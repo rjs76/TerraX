4177d39b31a001e36e6d523f979cfefe53b43398
Riviere <rjs76@njit.edugit config --global user.name Rivieregit config --global user.email rjs76@njit.edu>
2019-07-22 06:14:48 -0400

    inventory system

---
+++ inventory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventory : MonoBehaviour
+{
+    // to determine if the first inventory slot is full or not
+    public bool[] isFull;
+    public GameObject[] slots;
+    
+}
---
+++ pickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    private inventory inventory1;
+    public GameObject itemButton;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        inventory1 = GameObject.FindGameObjectsWithTag("Player").GetComponent<inventory>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            for(int i=0; i< inventory1.slots.Length; i++)
+            {
+                if(inventory1.isFull[i] == false)
+                {
+                    inventory1.isFull[i] = true;
+                    Instantiate(itemButton, inventory1.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+
+}
